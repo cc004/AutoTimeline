3604918e79e52e6e64c0afe0ecbf4e5bf6e8a7a2
qq1176321897 <1176321897@qq.com>
2021-04-29 15:50:50 +0800

    fix shell typo, add waitTillCrit api

--- PCRAutoTimeline/timeline.lua
+++ PCRAutoTimeline/timeline.lua
@@ -1,15 +1,15 @@
 -- scan character handles
---[[
+---[[
 local charas = {
-    autopcr.getUnitAddr(104701, 5, 12),
+    autopcr.getUnitAddr(104701, 5, 13),
+    autopcr.getUnitAddr(101701, 5, 15),
     autopcr.getUnitAddr(104301, 5, 13),
-    autopcr.getUnitAddr(102701, 5, 13),
-    autopcr.getUnitAddr(107101, 5, 12),
-    autopcr.getUnitAddr(170101, 5, 13)
+    autopcr.getUnitAddr(106301, 5, 15),
+    autopcr.getUnitAddr(103801, 5, 15)
 };
-]]
+--]]
 -- data for 1600x900
---[[ minitouch test
+---[[ minitouch test
 minitouch.connect("localhost", 1111);
 for i = 0, 4 do
     minitouch.setPos(5 - i, 400 + i * 208, 860);
@@ -25,15 +25,16 @@ for i = 1, 5 do
 end
 --]]
 
---[[ auto ub
+---[[ auto ub
 while (autopcr.getTime() > 1) --when not end
 do
-    for i = 1, 5 do --judge every chara
+    for i = 2, 5 do --judge every chara
         if (autopcr.getTp(charas[i]) == 1000) --ready for tp
         then
+            autopcr.waitTillCrit(charas[i], 85, false, 300 + autopcr.getFrame());
             print('trying to press '..i, 'frame='..autopcr.getFrame())
-            autopcr.framePress(i);
-            --minitouch.framePress(i); --trigger ub press
+            --autopcr.framePress(i);
+            minitouch.framePress(i); --trigger ub press
             break;
         end
     end
@@ -55,9 +56,8 @@ autopcr.waitFrame(4000);
 minitouch.press(5);
 --]]
 
----[[ crit test
+--[[ crit test
 local chara = autopcr.getUnitAddr(106301, 5, 15)
---]]
 local boss = 85;
 local last = 0;
 
@@ -76,6 +76,7 @@ do
         end
     end
 end
+--]]
 
 autopcr.waitTime(.5);
 minitouch.framePress(8);
